f5d9144c2080a9b0a2d0255b71e6afc87ecadef6
Batmanpouknight <irfn7pouyan@gmail.com>
2024-08-12 22:58:57 -0700

    added a page that allows admins to promote and demote users, database also stores username of users

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import crypto from 'crypto'
-import { error } from 'console'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import bcrypt from 'bcrypt'
 dotenv.config()
 
 const app = express()
@@ -10,24 +12,31 @@ const app = express()
 const mongodb_host = process.env.MONGODB_HOST
 const mongodb_user = process.env.MONGODB_USER
 const mongodb_password = process.env.MONGODB_PASSWORD
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}`
+
+const node_session_secret = process.env.NODE_SESSION_SECRET
+
 const database = new MongoClient(`${atlasURI}/?retryWrites=true`)
 const months = database.db('calender').collection('2024')
 
-const monthString = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
+const monthString = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+var mongoStore = MongoStore.create({
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+})
+
+app.use(
+  session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true,
+  })
+)
 
 // Middleware to parse JSON bodies
 app.use(express.json())
@@ -39,6 +48,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
+app.set('view engine', 'ejs')
 
 function generateRandomId() {
   return crypto.randomBytes(16).toString('hex') // Generates a 32-character random ID
@@ -50,7 +60,6 @@ app.get('/', (req, res) => {
 
 app.get('/months', async (req, res) => {
   const data = await months.find().sort({ _id: 1 }).toArray()
-  console.log('request for /months')
   res.json(data)
 })
 
@@ -62,197 +71,101 @@ app.post('/api/addevent', async (req, res) => {
 
   switch (dayIndex) {
     case 0:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.0.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.0.events': { id, name, description, country } } })
       break
     case 1:
       console.log('we in here', month)
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.1.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.1.events': { id, name, description, country } } })
       break
     case 2:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.2.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.2.events': { id, name, description, country } } })
       break
     case 3:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.3.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.3.events': { id, name, description, country } } })
       break
     case 4:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.4.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.4.events': { id, name, description, country } } })
       break
     case 5:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.5.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.5.events': { id, name, description, country } } })
       break
     case 6:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.6.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.6.events': { id, name, description, country } } })
       break
     case 7:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.7.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.7.events': { id, name, description, country } } })
       break
     case 8:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.8.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.8.events': { id, name, description, country } } })
       break
     case 9:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.9.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.9.events': { id, name, description, country } } })
       break
     case 10:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.10.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.10.events': { id, name, description, country } } })
       break
     case 11:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.11.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.11.events': { id, name, description, country } } })
       break
     case 12:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.12.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.12.events': { id, name, description, country } } })
       break
     case 13:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.13.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.13.events': { id, name, description, country } } })
       break
     case 14:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.14.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.14.events': { id, name, description, country } } })
       break
     case 15:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.15.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.15.events': { id, name, description, country } } })
       break
     case 16:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.16.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.16.events': { id, name, description, country } } })
       break
     case 17:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.17.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.17.events': { id, name, description, country } } })
       break
     case 18:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.18.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.18.events': { id, name, description, country } } })
       break
     case 19:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.19.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.19.events': { id, name, description, country } } })
       break
     case 20:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.20.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.20.events': { id, name, description, country } } })
       break
     case 21:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.21.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.21.events': { id, name, description, country } } })
       break
     case 22:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.22.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.22.events': { id, name, description, country } } })
       break
     case 23:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.23.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.23.events': { id, name, description, country } } })
       break
     case 24:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.24.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.24.events': { id, name, description, country } } })
       break
     case 25:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.25.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.25.events': { id, name, description, country } } })
       break
     case 26:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.26.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.26.events': { id, name, description, country } } })
       break
     case 27:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.27.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.27.events': { id, name, description, country } } })
       break
     case 28:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.28.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.28.events': { id, name, description, country } } })
       break
     case 29:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.29.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.29.events': { id, name, description, country } } })
       break
     case 30:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.30.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.30.events': { id, name, description, country } } })
       break
     case 31:
-      mongoResult = await months.updateOne(
-        { name: monthString[month] },
-        { $push: { 'dates.31.events': { id, name, description, country } } }
-      )
+      mongoResult = await months.updateOne({ name: monthString[month] }, { $push: { 'dates.31.events': { id, name, description, country } } })
       break
   }
 
@@ -261,21 +174,106 @@ app.post('/api/addevent', async (req, res) => {
   res.send(id)
 })
 
-app.post('/users/signup', async (req, res) => {
-  const { email, password } = req.body
-  console.log('request for /users/signup', email, password)
+app.get('/authenticate', async (req, res) => {
+  res.render('login')
+})
 
-  const userExists = await database.db('calender').collection('users').findOne({ email })
-  console.log(userExists)
-  if (userExists) {
+app.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body
+  console.log('request for /authenticate', email, password)
+
+  const user = await database.db('calender').collection('users').findOne({ email })
+  console.log(user)
+  if (!user) {
+    res.status(401)
+    res.redirect('/authenticate')
+    return
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    res.status(401)
+    res.redirect('/authenticate')
+    return
+  }
+
+  req.session.authenticated = true
+  req.session.user = user
+
+  res.redirect('/users')
+})
+
+app.get('/users', async (req, res) => {
+  if (!req.session.authenticated || req.session.user.type !== 'admin') {
+    res.redirect('/authenticate')
+    return
+  }
+
+  const data = await database.db('calender').collection('users').find().toArray()
+  res.render('users', { users: data })
+})
+
+app.post('/logout', async (req, res) => {
+  req.session.destroy()
+  res.redirect('/authenticate')
+})
+
+app.post('/users/promote', async (req, res) => {
+  const { email } = req.body
+  const user = await database.db('calender').collection('users').findOne({ email })
+  console.log(user)
+  if (!user) {
+    res.status(400)
+    res.send({ error: 'User not found' })
+    return
+  }
+  const result = await database
+    .db('calender')
+    .collection('users')
+    .updateOne({ email }, { $set: { type: 'admin' } })
+  res.status(200)
+  res.send({ success: true, result })
+})
+
+app.post('/users/demote', async (req, res) => {
+  const { email } = req.body
+
+  const user = await database.db('calender').collection('users').findOne({ email })
+  if (!user) {
+    res.status(400)
+    res.send({ error: 'User not found' })
+    return
+  }
+  const result = await database
+    .db('calender')
+    .collection('users')
+    .updateOne({ email }, { $set: { type: 'user' } })
+
+  res.status(200)
+  res.send({ success: true, result })
+})
+
+app.post('/users/signup', async (req, res) => {
+  const { email, username, password, type } = req.body
+  console.log('request for /users/signup', email, username, password, type)
+
+  const emailExists = await database.db('calender').collection('users').findOne({ email })
+  const usernameExists = await database.db('calender').collection('users').findOne({ username })
+  if (emailExists) {
     res.send({
       result: false,
-      error: { code: 409, message: 'User already exists', location: 'email' },
+      error: { code: 409, message: 'Email already exists', location: 'email' },
+    })
+    return
+  } else if (usernameExists) {
+    res.send({
+      result: false,
+      error: { code: 409, message: 'Username already exists', location: 'username' },
     })
     return
   }
 
-  const user = await database.db('calender').collection('users').insertOne({ email, password })
+  const user = await database.db('calender').collection('users').insertOne({ email, username, password, type })
   const id = user.insertedId
 
   res.status(200)
@@ -283,9 +281,10 @@ app.post('/users/signup', async (req, res) => {
 })
 
 app.post('/users/login', async (req, res) => {
-  const { email, password } = req.body
-  console.log('request for /users/login', email, password)
+  const { email } = req.body
+  console.log('request for /users/login', email)
 
+  //TODO: add a way that it searches for the email or username
   const user = await database.db('calender').collection('users').findOne({ email })
   console.log(user)
 
@@ -298,7 +297,12 @@ app.post('/users/login', async (req, res) => {
   }
   res.status(200)
   res.send({
-    result: { id: user._id.toHexString(), email: user.email, password: user.password },
+    result: {
+      id: user._id.toHexString(),
+      username: user.username,
+      email: user.email,
+      password: user.password,
+    },
     error: false,
   })
 })
